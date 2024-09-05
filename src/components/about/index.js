15cadfe19cfced445f15b9e6223114f091a46d6d
Ziming <yanzmics@qq.com>
2024-09-05 09:25:55 +0800

    Update index.js

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,7 +8,7 @@ const About = () => {
       <h1>About Me</h1>
       <div className='container'>
         <p>Ziming is a presidential postdoctoral fellow, with research interests for power systems operation, control, management, and machine learning.</p>
-        <p>Ziming’s profile include teaching and mentoring, academic publications (with ESI highly cited paper, Homepage Popular Articles in IEEE TWPRS, 730+ scholar citations, industrial projects (including SPGroup, National Environment Agency, Housing& Development Board, LITEON, State Grid of China), software systems, patents (11 patents + 6 TDs), and competition awards in IEEE and NeurIPS.</p>
+        <p>Ziming’s profile include teaching and mentoring, academic publications (with 2 ESI highly cited paper, 850+ scholar citations, industrial projects (including SPGroup, National Environment Agency, Housing& Development Board, LITEON, State Grid of China, ComfortDelgro), software systems, patents (11 patents + 9 TDs), and competition awards in IEEE and NeurIPS.</p>
         <p>Ziming also serves as IEEE outstanding reviewers and IEEE Singapore Chapter committee member.</p>
       </div>
     </div>
